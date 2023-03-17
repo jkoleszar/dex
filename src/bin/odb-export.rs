0b14722a5f0e5a43f1844f0951c9ec34504b6c4e
John Koleszar <jkoleszar@gmail.com>
2023-03-17 14:19:10 -0400

    odb-export: factor out single oid export
    This is in preparation for odb-export supporting a server mode that exports
    on demand.

--- src/bin/odb-export.rs
+++ src/bin/odb-export.rs
@@ -18,26 +18,30 @@ struct Args {
     oid: String,
 }
 
+async fn export_one(conn: Connection, oid: ObjectId) -> Result<()> {
+    let export: export::Client = capnp_rpc::new_client(Export::new(conn));
+    let importer: import::Client = capnp_rpc::new_client(ImportToStdout);
+
+    let mut want_req = export.want_request();
+    want_req.get().init_id().from_oid(&oid);
+    want_req.send().promise.await?;
+
+    let mut begin_req = export.begin_request();
+    begin_req.get().set_import(importer);
+    begin_req.send().promise.await?;
+
+    Ok(())
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     let args = Args::parse();
-    let oid = ObjectId::parse(&args.oid)?;
 
     tokio::task::LocalSet::new()
         .run_until(async move {
             let conn = Connection::open(&args.db).await?;
             conn.call(|conn| ObjectDb::new(conn).create()).await?;
-            let export: export::Client = capnp_rpc::new_client(Export::new(conn));
-            let importer: import::Client = capnp_rpc::new_client(ImportToStdout);
-
-            let mut want_req = export.want_request();
-            want_req.get().init_id().from_oid(&oid);
-            want_req.send().promise.await?;
-
-            let mut begin_req = export.begin_request();
-            begin_req.get().set_import(importer);
-            begin_req.send().promise.await?;
-
+            export_one(conn, ObjectId::parse(&args.oid)?).await?;
             Ok(())
         })
         .await
