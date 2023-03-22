3cacf69f4ccb78a9c711407f8010740b164cbfc3
John Koleszar <jkoleszar@gmail.com>
2023-03-22 14:41:45 -0400

    log: add LogSlowFuture WIP
    It might be nice to silence some of the log spam that's only there
    because we'd like to know where a task is stuck. This is the start of
    that, but is not quite working yet.

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,10 @@
 use std::fmt::Display;
+use std::future::Future;
+use std::marker::Unpin;
+use std::pin::Pin;
+use std::time::Duration;
+
+use async_trait::async_trait;
 
 pub trait LogIfError<T, E: Display>: Into<Result<T, E>> {
     fn log_if_error(self, context: &str) -> Result<T, E> {
@@ -10,3 +16,23 @@ pub trait LogIfError<T, E: Display>: Into<Result<T, E>> {
     }
 }
 impl<T, E: Display> LogIfError<T, E> for Result<T, E> {}
+
+#[async_trait]
+pub trait LogSlowFuture<T>: Future<Output = T> + Unpin {
+    async fn log_slow<'a>(
+        &'a mut self,
+        timeout: Duration,
+        message: &'static str,
+    ) -> Pin<Box<dyn Future<Output = T> + 'a>> {
+        Box::pin(async move {
+            let printer = tokio::spawn(async move {
+                tokio::time::sleep(timeout).await;
+                log::info!("still waiting: {message}");
+            });
+            let r = self.await;
+            printer.abort();
+            r
+        })
+    }
+}
+impl<T, U: Future<Output = T> + Unpin> LogSlowFuture<T> for U {}
