0fd90b8e64b43a843ff3b295e10c2b1c2f7a1c0c
John Koleszar <jkoleszar@gmail.com>
2023-03-27 15:23:11 -0400

    fuse: load stat eagerly
    The lazy-loading of stat information doesn't work because some FUSE
    metheods are sync, and we can't call our async loading function in that
    context. Instead, stat data will always be valid, which should be
    sufficient to get readdir working.

--- src/fuse.rs
+++ src/fuse.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
 
-use anyhow::{anyhow, Context as AnyhowContext, Result};
+use anyhow::{anyhow, bail, Context as AnyhowContext, Result};
 use async_trait::async_trait;
 use fuse_backend_rs::abi::fuse_abi::{CreateIn, OpenOptions, SetattrValid};
 use fuse_backend_rs::api::filesystem::{
@@ -20,7 +20,7 @@ use tokio::sync::Mutex as AsyncMutex;
 use tokio::task::JoinHandle;
 use tokio_util::sync::CancellationToken;
 
-use crate::odb::{ObjectId, StatWithUnknownInode};
+use crate::odb::{ObjectId, ObjectReader, StatWithUnknownInode};
 use crate::odb_readthrough;
 use crate::odb_readthrough::LazyObject;
 use crate::proto::odb_capnp::{object, tree};
@@ -43,12 +43,12 @@ pub enum InodeError {
 /// An InodeEntry that may be shared between threads.
 struct InodeEntry {
     oid: ObjectId,
-    object: LazyObject,
+    object: AsyncMutex<LazyObject>,
     parent_inode: u64,
-    stat: Option<libc::stat64>,
+    stat: libc::stat64,
 }
 
-type SharedInodeEntry = Arc<AsyncMutex<InodeEntry>>;
+type SharedInodeEntry = Arc<InodeEntry>;
 
 struct InodeTable(RwLock<Vec<Option<SharedInodeEntry>>>);
 impl InodeTable {
@@ -63,7 +63,8 @@ impl InodeTable {
     }
 
     fn get(&self, inode: u64) -> Result<SharedInodeEntry> {
-        Ok(self.0
+        Ok(self
+            .0
             .read()
             .unwrap() // assume the lock was not poisoned.
             .get(Self::index(inode)?)
@@ -85,49 +86,81 @@ impl InodeTable {
     fn insert(&self, entry: InodeEntry) -> usize {
         let mut vec = self.0.write().unwrap();
         let ino = vec.len();
-        vec.push(Some(Arc::new(AsyncMutex::new(entry))));
+        vec.push(Some(Arc::new(entry)));
         ino
     }
 
-    fn replace(
-        &self,
-        index: usize,
-        entry: InodeEntry,
-    ) {
+    fn replace(&self, index: usize, entry: InodeEntry) {
         let mut vec = self.0.write().unwrap();
-        vec[index] = Some(Arc::new(AsyncMutex::new(entry)));
+        vec[index] = Some(Arc::new(entry));
     }
 }
+
+type OdbSender = mpsc::Sender<odb_readthrough::Request>;
 
 /// DexFS state shared across all workers
 struct SharedState {
     /// Connection to object database
-    odb: mpsc::Sender<odb_readthrough::Request>,
+    odb: OdbSender,
 
     /// Indexed by inode
     inodes: InodeTable,
 }
 
 impl SharedState {
-    fn new(odb: mpsc::Sender<odb_readthrough::Request>, root: ObjectId) -> Arc<SharedState> {
+    fn new(odb: OdbSender) -> Arc<SharedState> {
         let state = SharedState {
             odb: odb.clone(),
             inodes: InodeTable::new(),
         };
 
+        Arc::new(state)
+    }
+
+    async fn fetch_object<'a>(
+        object: &'a mut LazyObject,
+        odb: &OdbSender,
+    ) -> Result<&'a ObjectReader> {
+        match object.get(odb).await {
+            LazyObject::Error(e) => {
+                // TODO: LazyObject::Error currently has a restricted lifetime. This
+                // may go away if that module ends up switching to use anyhow.
+                Err(anyhow!("LazyObject::Error: {e:?}"))
+            }
+            LazyObject::Missing(oid) => {
+                Err(FuseError::new(ENOENT)).context(format!("missing oid {oid}"))
+            }
+            LazyObject::Ok(o) => Ok(o),
+            LazyObject::Future(_) | LazyObject::Pending(_) => unreachable!(),
+        }
+    }
+
+    async fn populate_root(&self, root: ObjectId) -> Result<()> {
+        let mut lazy_object = LazyObject::new(root);
+        let object = Self::fetch_object(&mut lazy_object, &self.odb)
+            .await
+            .with_context(|| format!("populate_root({root})"))?;
+        let reader = object.reader();
+        let (tree, stat) = if let object::TreeRoot(root) = reader.get()?.which()? {
+            let stat = StatWithUnknownInode::try_from(root.clone()?.get_stat()?)?.finalize(1);
+            (root?.get_tree()?, stat)
+        } else {
+            bail!("oid {root} is not a TreeRoot");
+        };
+
         // inode 0 is unused
-        state.inodes.alloc();
+        self.inodes.alloc();
 
         // inode 1 is the root object
-        let entry = InodeEntry {
+        self.inodes.insert(InodeEntry {
             oid: root.clone(),
-            object: LazyObject::new(root),
+            object: lazy_object.into(),
             parent_inode: 0,
-            stat: None,
-        };
-        state.inodes.insert(entry);
+            stat: stat,
+        });
+        self.populate_tree(tree, 1)?;
 
-        Arc::new(state)
+        Ok(())
     }
 
     fn populate_tree<'a>(
@@ -154,9 +187,9 @@ impl SharedState {
                     idx,
                     InodeEntry {
                         oid: oid.clone(),
-                        object: LazyObject::new(oid),
+                        object: LazyObject::new(oid).into(),
                         parent_inode: parent,
-                        stat: Some(stat),
+                        stat,
                     },
                 );
                 Ok(idx)
@@ -166,13 +199,14 @@ impl SharedState {
     }
 
     async fn get_inode(&self, inode: u64) -> Result<SharedInodeEntry, anyhow::Error> {
-        let shared_entry = self.inodes.get(inode)?;
+        use std::ops::Deref;
+        let entry = self.inodes.get(inode)?;
 
-        let mut entry = shared_entry.lock().await;
+        let mut object_lock = entry.object.lock().await;
 
         // Check that the entry is valid.
-        let initially_ok = matches!(entry.object, LazyObject::Ok(_));
-        let object = match entry.object.get(&self.odb).await {
+        let initially_ok = matches!(object_lock.deref(), LazyObject::Ok(_));
+        let object = match object_lock.get(&self.odb).await {
             LazyObject::Error(e) => {
                 // TODO: LazyObject::Error currently has a restricted lifetime. This
                 // may go away if that module ends up switching to use anyhow.
@@ -187,34 +221,13 @@ impl SharedState {
         // Populate our child inodes if necessary.
         if !initially_ok {
             let reader = object.reader();
-            let which = reader
-                .get()
-                .map_err(|e| {
-                    anyhow::Error::from(e)
-                        .context(format!("corrupt object at ino {inode} oid {}", entry.oid))
-                })?
-                .which()
-                .map_err(|e| {
-                    anyhow::Error::from(e).context(format!(
-                        "unknown object type at ino {inode} oid {}",
-                        entry.oid
-                    ))
-                })?;
-
-            // Extract stat for tree root.
-            let tree = match which {
-                object::TreeRoot(root) => {
-                    let stat =
-                        StatWithUnknownInode::try_from(root.clone()?.get_stat()?)?.finalize(inode);
-                    entry.stat = Some(stat);
-                    Some(root?.get_tree())
-                }
-                object::Tree(tree) => Some(tree),
-                _ => None,
-            };
 
             // Populate tree entries.
-            if let Some(tree) = tree {
+            if let Some(tree) = match reader.get()?.which()? {
+                object::TreeRoot(root) => Some(root?.get_tree()),
+                object::Tree(tree) => Some(tree),
+                _ => None,
+            } {
                 tree.map_err(anyhow::Error::from)
                     .and_then(|tree| Ok(self.populate_tree(tree, entry.parent_inode)?))
                     .map_err(|e| {
@@ -224,21 +237,8 @@ impl SharedState {
             }
         }
 
-        // TODO: It seems like we can't return a tokio MutexGuard like you can
-        // an ordinary sync mutex? The caller will have to re-lock.
-        drop(entry);
-        Ok(shared_entry)
-    }
-
-    async fn stat(&self, inode: u64) -> anyhow::Result<libc::stat64> {
-        let shared_child = self.get_inode(inode).await?;
-        let child = shared_child.lock().await;
-        if let Some(stat) = child.stat {
-            return Ok(stat.clone().into());
-        }
-        // Stat has not been populated.
-
-        Err(anyhow!("inode {inode} has unset stat structure."))
+        drop(object_lock);
+        Ok(entry)
     }
 }
 
@@ -321,14 +321,14 @@ impl AsyncFileSystem for DexFS {
         inode: <Self as FileSystem>::Inode,
         _handle: Option<<Self as FileSystem>::Handle>,
     ) -> io::Result<(libc::stat64, Duration)> {
-        let result = self
+        let entry = self
             .shared
-            .stat(inode)
+            .get_inode(inode)
             .await
-            .map(|stat| (stat, TTL))
-            .map_err(FuseError::from);
+            .map_err(FuseError::from)?;
+        let result = (entry.stat.clone(), TTL);
         log::trace!("async_getattr: {:?}", result);
-        Ok(result?)
+        Ok(result)
     }
 
     async fn async_setattr(
@@ -455,14 +455,15 @@ async fn service_kernel(server: FuseServer, mut channel: FuseChannel) -> Result<
 pub async fn run_fuse(
     mount_point: &Path,
     task_count: usize,
-    odb: mpsc::Sender<odb_readthrough::Request>,
+    odb: OdbSender,
     root: ObjectId,
     until: CancellationToken,
-) -> Result<(), Error> {
+) -> Result<(), anyhow::Error> {
     let mut tasks: Vec<JoinHandle<Result<(), Error>>> = Vec::new();
 
     // Initialize state shared across all workers.
-    let fs_shared_state = SharedState::new(odb, root);
+    let fs_shared_state = SharedState::new(odb);
+    fs_shared_state.populate_root(root).await?;
 
     // Create FUSE session. Dropping this handle will unmount the filesystem.
     let mut fuse_session = FuseSession::new(
